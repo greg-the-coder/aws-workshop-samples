6abe542ea756539d290a68e416c90ecbefb5e43c
greg-the-coder <greg@coder.com>
2025-02-25 22:55:19 +0000

    updated comments for clarity

--- coder-admin/deploy2eks.sh
+++ coder-admin/deploy2eks.sh
@@ -1,4 +1,5 @@
-# Documnet the scripts/options used for simple AWS EKS Deployment of Coder for Demo's and Workshops
+# Document the scripts/options used for simple AWS EKS Deployment of Coder for Demo's and Workshops
+# Prereqs: AWS Account/Access with appropriate permissions and latest AWS, eksctl, kubectl, helm cli's installed.
 
 # Create EKS Cluster using default eksctl generated supporting resources, and leverage auto-mode features for simplicity (change cluster name to your own)
 eksctl create cluster --name=gtc-test-podid-eks --enable-auto-mode --region us-east-2
@@ -22,14 +23,33 @@ EOF
 
 # Create Coder Namespace and Deploy In-Cluster PostgreSQL - https://coder.com/docs/install/kubernetes
 kubectl create namespace coder
+# Install PostgreSQL
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm install coder-db bitnami/postgresql \
+    --namespace coder \
+    --set auth.username=coder \
+    --set auth.password=coder \
+    --set auth.database=coder \
+    --set persistence.size=10Gi
 
-# Install Coder using Helm and supplied coder-core-values-v2.yaml base (replace coder_access_url values with your own)
+#Create secret used by std Coder Deployment
+kubectl create secret generic coder-db-url -n coder \
+  --from-literal=url="postgres://coder:coder@coder-db-postgresql.coder.svc.cluster.local:5432/coder?sslmode=disable"
+
+# Install Coder using Helm and supplied coder-core-values-v2.yaml base 
+helm repo add coder-v2 https://helm.coder.com/v2
 helm install coder coder-v2/coder \
     --namespace coder \
     --values coder-core-values-v2.yaml \
     --version 2.19.0
 
-# Create IAM Role & Trust Relationship for EC2 Workspace Support (change rolename to your own)
+# Perform helm upgrade to update Coder with actual K8S Service endpoints created for use with CODER_ACCESS_URL and CODER_WILDCARD_ACCESS_URL after updating coder-core-values-v2.yaml
+helm upgrade coder coder-v2/coder \
+    --namespace coder \
+    --values coder-core-values-v2.yaml \
+    --version 2.19.0
+
+# Create IAM Role & Trust Relationship for EC2 Workspace Support (change rolename to your own name)
 aws iam create-role --role-name gtc-coder-ec2-workspace-eks-role --assume-role-policy-document file://ekspodid-trust-policy.json
 aws iam attach-role-policy \
     --role-name gtc-coder-ec2-workspace-eks-role \
@@ -38,7 +58,7 @@ aws iam attach-role-policy \
     --role-name gtc-coder-ec2-workspace-eks-role \
     --policy-arn arn:aws:iam::aws:policy/IAMReadOnlyAccess
 
-# Add and IAM Pod Identify association for EC2 Workspace support (change cluster name to your own)
+# Add and IAM Pod Identify association for EC2 Workspace support (change cluster and role name to your own)
 aws eks create-pod-identity-association \
     --cluster-name gtc-test-podid-eks \
     --namespace coder \
